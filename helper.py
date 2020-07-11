69fec04e24689252aef4d38030d4ddf817e2046c
Kwang Ho Hur <hurxx018@gmail.com>
2020-07-11 11:50:53 -0500

    docs: Add a document for load_data, style: Chang the input format

--- helper.py
+++ helper.py
@@ -2,8 +2,16 @@ import os
 
 
 
-def load_data(filename):
+def load_data(
+    filename
+    ):
+    """ Load data from file
+        Arguments:
+        filename : a string of filename
 
+        Returns:
+        a text string
+    """
     path_name = os.path.join(filename)
     with open(path_name, 'r') as f:
         text = f.read()
