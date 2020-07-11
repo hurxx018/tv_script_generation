eea86c90d18ff957b2b2b0a71a35c67b2377cb04
Kwang Ho Hur <hurxx018@gmail.com>
2020-07-11 08:49:45 -0500

    docs: Add a document of create_lookup_table in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,14 @@
 def create_lookup_table(
     text
     ):
-    """
+    """Create Lookup Tables for Vocabulary
+       Arguments
+       ---------
+       text: a list of words 
+
+       Return
+       ------
+       a tuple of two dicts (vocab_to_int, int_to_vocab)
     """
     unique_words = set(text)
     int_to_vocab = dict(enumerate(unique_words))
