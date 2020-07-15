2ad3b8c30640244927dc5dd5ad24b55d2a72942b
Kwang Ho Hur <hurxx018@gmail.com>
2020-07-15 13:10:20 -0500

    test: Finalize test.py

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 import unittests
-
+import torch
 from preprocess import create_lookup_table, token_lookup
-from neural_networks import batch_data
+from neural_networks import batch_data, RNN, forward_back_prop
 
 unittests.test_create_lookup_table(create_lookup_table)
 
 unittests.test_token_lookup(token_lookup)
 
 unittests.test_batch_data(batch_data)
+
+train_on_gpu = torch.cuda.is_available()
+unittests.test_rnn(RNN, train_on_gpu)
+
+unittests.test_forward_back_prop(RNN, forward_back_prop, train_on_gpu)
