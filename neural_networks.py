4440cc5b24413e8d513809a6098e7df9837c15ad
Kwang Ho Hur <hurxx018@gmail.com>
2020-07-19 12:33:19 -0500

    feat: Move a validation step to the end of a single epoch of training

--- neural_networks.py
+++ neural_networks.py
@@ -268,38 +268,44 @@ def train(
             # print training loss stats and evaluate the validation set.
             if (batch_i % show_every_n_batches == 0):
 
-                # Evaluate the Validation dataset
-                rnn.eval()
-                valid_losses = []
-                valid_hidden = rnn.init_hidden(batch_size, train_on_gpu)
-                with torch.no_grad():
-                    for valid_batch_j, (inputs, targets) in enumerate(valid_loader, 1):
-                        # Make sure that the size of valid_inputs equals batch_size.
-                        if len(inputs) != batch_size:
-                            break
-                        if train_on_gpu:
-                            inputs, targets = inputs.cuda(), targets.cuda()
-
-                        valid_hidden = tuple([h.detach() for h in valid_hidden])
-                        outputs, valid_hidden = rnn(inputs, valid_hidden)
-                        loss = criterion(outputs, targets)
-
-                        valid_losses.append(loss.item())
-
-                mean_valid_losses = np.mean(valid_losses)
-                print("Epoch: {:>4}/{:<4}....Step: {}....Train_Loss: {}....Valid_Loss: {}....Min Valid Loss: {}\n".format(
-                    epoch_i, n_epochs, batch_i, np.mean(batch_losses), mean_valid_losses, min_validation_loss
+                print("Epoch: {:>4}/{:<4}....Step: {}....Train_Loss: {}\n".format(
+                    epoch_i, n_epochs, batch_i, np.mean(batch_losses)
                 ))
+                batch_losses = []
 
-                if min_validation_loss > mean_valid_losses:
-                    min_validation_loss = mean_valid_losses
-                    if isinstance(savecheckpoint, str):
-                        print("Saving the RNN model.\n")
-                        save_rnn_model(savecheckpoint, rnn)
+        # Evaluate the Validation dataset
+        rnn.eval()
+        valid_losses = []
 
+        with torch.no_grad():
+            for valid_batch_j, (inputs, targets) in enumerate(valid_loader, 1):
 
-                batch_losses = []                
-                rnn.train()
+                valid_hidden = rnn.init_hidden(batch_size, train_on_gpu)
+
+                # Make sure that the size of valid_inputs equals batch_size.
+                if len(inputs) != batch_size:
+                    break
+                if train_on_gpu:
+                    inputs, targets = inputs.cuda(), targets.cuda()
+
+                valid_hidden = tuple([h.detach() for h in valid_hidden])
+                outputs, valid_hidden = rnn(inputs, valid_hidden)
+                loss = criterion(outputs, targets)
+
+                valid_losses.append(loss.item())
+
+        mean_valid_losses = np.mean(valid_losses)
+        print("Epoch: {:>4}/{:<4}....Valid_Loss: {}\n".format(
+            epoch_i, n_epochs, mean_valid_losses
+        ))
+
+        if min_validation_loss > mean_valid_losses:
+            min_validation_loss = mean_valid_losses
+            if isinstance(savecheckpoint, str):
+                print("Saving the RNN model.\n")
+                save_rnn_model(savecheckpoint, rnn)
+                                
+        rnn.train()
 
     # returns a trained rnn
     return rnn
